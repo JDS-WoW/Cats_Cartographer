44203b7a621212d65ab9375642d927f7f8919bac
Jennifer Snow <Catreina@users.noreply.github.com>
2021-07-14 21:16:10 -0400

    Modified TOC files
    Added expansion-specific TOC files and an expansion check command

--- Init.lua
+++ Init.lua
@@ -1,1 +1,12 @@
-local addon = ...
+-- Set up our defaults for this addon and the Namespace we are running within
+
+-- Get the addon name (Cats_Cartographer) and the lua files shared addon table (Namespace)
+local addon, Namespace = ...
+local CatsCart = { } 
+
+
+function CatsCart:GetExpansion()
+    message(GetExpansionLevel())
+end
+
+-- CatsCart:GetExpansion()
